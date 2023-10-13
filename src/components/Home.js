b6404f595b2f47dc1502229956d2fff6698afd07
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-12 20:38:16 -0700

    add load more btn and details

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [visiblePosts, setVisiblePosts] = useState(10);
 
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -15,16 +17,37 @@ const Home = () => {
       });
   }, []);
 
+  const handlePostClick = (post) => {
+    setSelectedPost(post);
+  };
+
+  const loadMorePosts = () => {
+    setVisiblePosts(visiblePosts + 5); // add each five articles
+  }
+
+
   return (
     <div>
       <h1>Blog Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <a href={`/post/${post.id}`}>{post.title}</a>
+      {selectedPost && (
+        <div>
+          <h2>{selectedPost.title}</h2>
+          <p>{selectedPost.body}</p>
+        </div>
+      )}
+
+<ul>
+        {posts.slice(0, visiblePosts).map((post) => (
+          <li key={post.id} onClick={() => handlePostClick(post)}>
+            {post.title}
           </li>
         ))}
       </ul>
+
+      {visiblePosts < posts.length && (
+        <button onClick={loadMorePosts}>Load More</button>
+      )}
+
     </div>
   );
 };
