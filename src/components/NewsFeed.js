21e3d0b1e689d8757c04f80cbeb479838ef7940c
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-18 17:09:55 -0700

    add the search bar

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -5,11 +5,17 @@ class NewsFeed extends Component {
     super();
     this.state = {
       articles: [],
+      searchTerm: '',
     };
   }
 
+  // Get the initialized data
   componentDidMount() {
-    // GET the data from NewsAPI
+    this.fetchNewsData();
+  }
+
+  // Get the data from NewsAPI
+  fetchNewsData = () => {
     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=102ed06f208b4f80a239b2703f4b1d21')
       .then(response => response.json())
       .then(data => {
@@ -18,14 +24,48 @@ class NewsFeed extends Component {
       .catch(error => {
         console.error('APIRequestError:', error);
       });
-  }
+  };
+
+  // Handler of changing the search keyword
+  handleSearchChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  // Handler of serching
+  handleSearch = () => {
+    const { articles, searchTerm } = this.state;
+
+    // Filter the article
+    const filteredArticles = articles.filter(article =>
+      article.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    // Reset the article that is filterd
+    this.setState({ articles: filteredArticles });
+  };
+
+  // Handler of resetting the serch
+  handleResetSearch = () => {
+    this.fetchNewsData();
+    this.setState({ searchTerm: '' });
+  };
 
   render() {
-    const { articles } = this.state;
+    const { articles, searchTerm } = this.state;
 
     return (
       <div>
         <h1>News Feed</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search for news..."
+            value={searchTerm}
+            onChange={this.handleSearchChange}
+          />
+          <button onClick={this.handleSearch}>Search</button>
+          <button onClick={this.handleResetSearch}>Reset</button>
+        </div>
         <ul>
           {articles.map((article, index) => (
             <li key={index}>
