e30a6442f84e4b3994081d6410bacbf50e53f04d
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-18 18:00:03 -0700

    install and set tailwindcss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tailwindcss/base.css'; // ベーススタイル
+import 'tailwindcss/components.css'; // コンポーネントスタイル
+import 'tailwindcss/utilities.css'; // ユーティリティスタイル
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
