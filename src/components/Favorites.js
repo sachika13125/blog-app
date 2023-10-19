2063239c22eee7e276535346e1efa2c9c2a4ed2f
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-18 17:24:47 -0700

    add Favorite.js

---
+++ src/components/Favorites.js
@@ -0,0 +1,35 @@
+class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favorites: [],
+    };
+  }
+
+  componentDidMount() {
+    // Get favorite articles from local storage
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    this.setState({ favorites });
+  }
+
+  render() {
+    const { favorites } = this.state;
+
+    return (
+      <div>
+        <h1>Favorites Page</h1>
+        <ul>
+          {favorites.map((article, index) => (
+            <li key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.description}</p>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                Read the Deatails
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
