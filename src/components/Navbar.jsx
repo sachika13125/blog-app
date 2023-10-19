bc222ac568427e50b027929ba8c89b44b8831584
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-19 16:37:20 -0700

    install styled-components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 function Navbar() {
-  return (
-    <div>
 
-        <ul>
-            <li>
-                <a href='/'>Feeds</a>
-            </li>
-            <li>
-                <a href='/favorites'>Favorites</a>
-            </li>
-            <li>
-                <a href='/login'>LogIn</a>
-            </li>
-        </ul>
-    </div>
+    const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 4rem;
+    background-color: skyblue;
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        aline-item: center;
+        list-style: none;
+    }
+    
+    li {
+        margin: 0 1rem; 
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+        font-size: 1.2rem;
+    }
+
+    `
+
+  return (
+    <Header>
+        <div>
+            <a href='#'>Home</a>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href='/'>Feeds</a>
+                </li>
+                <li>
+                    <a href='/favorites'>Favorites</a>
+                </li>
+                <li>
+                    <a href='/login'>LogIn</a>
+                </li>
+            </ul>
+        </nav>
+    </Header>
   );
 }
 
