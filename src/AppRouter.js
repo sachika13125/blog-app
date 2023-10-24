0befc2918b2076444bc5ef47d2dbe40b3aba617b
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2023-10-23 20:41:01 -0700

    add first view

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,17 +1,18 @@
-// src/AppRouter.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NewsFeed from './components/NewsFeed';
 import Favorites from './components/Favorites';
 import Login from './components/Login';
+import Home from './components/Home';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<NewsFeed />} />
+        <Route path="/newsfeeds" element={<NewsFeed />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
